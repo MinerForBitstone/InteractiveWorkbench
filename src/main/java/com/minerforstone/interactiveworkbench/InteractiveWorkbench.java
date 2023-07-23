3dfbf90aa8966fbcc5a475b834a9f152c42c2e53
MinerForBitstone <rayegner05@gmail.com>
2023-07-23 17:31:13 -0500

    Initial Commit

---
+++ src/main/java/com/minerforstone/interactiveworkbench/InteractiveWorkbench.java
@@ -0,0 +1,13 @@
+package com.minerforstone.interactiveworkbench;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InteractiveWorkbench implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
